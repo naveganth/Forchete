985d91e787c23caea3795cea1f47656ca7ff446b
Gabriel-1201 <pessoal@gabrielataide.com>
2025-04-30 19:13:02 -0300

    Relação básica da api com o scraper

--- backend/api/src/main.rs
+++ backend/api/src/main.rs
@@ -1,27 +1,63 @@
-use scraper::utils;
 use chrono::NaiveDate;
 use db;
+use axum::{
+    routing::get,
+    Router,
+};
+use std::time::Duration;
+use std::thread;
+use std::env;
 
 type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
 
-fn main() -> Result<()>{
+#[tokio::main]
+async fn main() -> Result<()> {
+    dotenvy::dotenv()?;
+    let intervalo = env::var("MINUTOS_INTERVALO_BUSCA")?.parse::<u64>()?;
+
     let pool = db::criar_pool()?;
+    
+    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    
+    thread::spawn(move || {
+        loop {
+            println!("Scraper: Iniciando tarefas");
+            let conn = &mut match pool.get_conn() {
+                Ok(a) => { a },
+                Err(e) => {
+                    println!("Scraper: Erro ao pegar conexão com o banco de dados: {}", e);
+                    continue;
+                }
+            };
+            let lista_noticias = match scraper::puxar_noticias() {
+                Ok(a) => { a },
+                Err(e) => {
+                    println!("Scraper: Erro ao listar notícias: {}", e);
+                    continue;
+                }
+            };
 
-    // let lista_noticias = scraper::puxar_noticias()?;
-    // scraper::salvar_noticias(lista_noticias, &mut pool.get_conn()?);
+            scraper::salvar_noticias(lista_noticias, conn);
 
-    let a = db::pegar_noticias(
-        &mut pool.get_conn()?, 
-        NaiveDate::parse_from_str("2025-04-26", "%Y-%m-%d")?, 
-        NaiveDate::parse_from_str("2025-04-26", "%Y-%m-%d")?, 
-        99, 
-        0
-    )?;
+            println!("Scraper: Tudo feito aqui patrão. Esperando {} minutos...", &intervalo);
+            thread::sleep(Duration::from_secs(intervalo * 60));
+        }
+    });
 
-    for i in a {
-        println!("Noticia encontrada: {:?}", i);
-    }
-    // println!("Notícias encontradas");
+    axum::serve(listener, app).await.unwrap();
+    
+    // let a = db::pegar_noticias(
+    //     &mut pool.get_conn()?, 
+    //     NaiveDate::parse_from_str("2025-04-26", "%Y-%m-%d")?, 
+    //     NaiveDate::parse_from_str("2025-04-26", "%Y-%m-%d")?, 
+    //     99, 
+    //     0
+    // )?;
+
+    // for i in a {
+    //     println!("Noticia encontrada: {:?}", i);
+    // }
 
     Ok(())
 }
