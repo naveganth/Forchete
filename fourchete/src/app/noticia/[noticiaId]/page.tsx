88efbda2a11484945cd135be6a3e32065cd8e5aa
Naveganth <lucas.navegant@gmail.com>
2025-05-02 16:39:01 -0300

    Testing for today

--- fourchete/src/app/noticia/[noticiaId]/page.tsx
+++ fourchete/src/app/noticia/[noticiaId]/page.tsx
@@ -1,3 +1,12 @@
-export default function teste() {
-    return <h1>Hello Mundo</h1>
-  }
+const noticias = async ({ params }) => {
+  const resposta = await fetch(
+    `https://jsonplaceholder.typicode.com/posts/${params.noticiaId}`
+  );
+  const dadosNoticia = await response.json();
+};
+
+const page = ({ params }) => {
+  return <div>ID da noticia: {params.noticiaId}</div>;
+};
+
+export default page;
