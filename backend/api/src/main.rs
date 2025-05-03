7467ef39389d27595022c36ef159b141abcf4e13
Gabriel-1201 <pessoal@gabrielataide.com>
2025-05-03 10:51:50 -0300

    Lógica pra fazer tudo ao mesmo tempo

--- backend/api/src/main.rs
+++ backend/api/src/main.rs
@@ -3,10 +3,16 @@ use db;
 use axum::{
     routing::get,
     Router,
+    extract::{Query, State},
+    response::Json,
 };
 use std::time::Duration;
 use std::thread;
 use std::env;
+use serde_json::{Value, json};
+use serde::{Deserialize, Serialize};
+use std::sync::Arc;
+use mysql::Pool;
 
 type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
 
@@ -14,16 +20,18 @@ type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
 async fn main() -> Result<()> {
     dotenvy::dotenv()?;
     let intervalo = env::var("MINUTOS_INTERVALO_BUSCA")?.parse::<u64>()?;
-
-    let pool = db::criar_pool()?;
+    let pool_thread = db::criar_pool()?;
+    let pool_api = Arc::new(db::criar_pool()?);
     
-    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+    let app = Router::new()
+        .route("/pegar_noticias", get(pegar_noticias))
+        .with_state(pool_api);
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     
     thread::spawn(move || {
         loop {
             println!("Scraper: Iniciando tarefas");
-            let conn = &mut match pool.get_conn() {
+            let conn = &mut match pool_thread.get_conn() {
                 Ok(a) => { a },
                 Err(e) => {
                     println!("Scraper: Erro ao pegar conexão com o banco de dados: {}", e);
@@ -46,18 +54,46 @@ async fn main() -> Result<()> {
     });
 
     axum::serve(listener, app).await.unwrap();
-    
-    // let a = db::pegar_noticias(
-    //     &mut pool.get_conn()?, 
-    //     NaiveDate::parse_from_str("2025-04-26", "%Y-%m-%d")?, 
-    //     NaiveDate::parse_from_str("2025-04-26", "%Y-%m-%d")?, 
-    //     99, 
-    //     0
-    // )?;
-
-    // for i in a {
-    //     println!("Noticia encontrada: {:?}", i);
-    // }
 
     Ok(())
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+struct QueryNoticias {
+    regiao: Option<String>, 
+    data_inicio: NaiveDate, 
+    data_fim: NaiveDate, 
+    quantidade: u32, 
+    offset: Option<u32>
+}
+
+async fn pegar_noticias(State(pool): State<Arc<Pool>>, query: Query<QueryNoticias>) -> Json<Value>{
+    let Ok(conn) = &mut pool.get_conn() else { return Json(json!("Banco de dados inacessível")) };
+    println!("Api: Busca de notícias com query: {:?}", query);
+
+    let regiao = match &query.regiao {
+        Some(a) => { a.clone() },
+        None => String::new()
+    };
+
+    let offset = match &query.offset {
+        Some(a) => { a.clone() },
+        None => { 0 }
+    };
+    
+    let noticias = match db::pegar_noticias(
+        conn, 
+        regiao, 
+        query.data_inicio, 
+        query.data_fim, 
+        query.quantidade,
+        offset
+    ) {
+        Ok(a) => {a},
+        Err(e) => {return Json(json!(format!("{}", e)))}
+    };
+
+    println!("Api: Notícias encontradas: {:?}", &noticias);
+
+    Json(json!(&noticias))
 }
