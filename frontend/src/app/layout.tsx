0364f1cda61e3829d23e0e40712497c79d9ffc35
Naveganth <lucas.navegant@gmail.com>
2025-05-09 16:38:28 -0300

    removendo metadata desnecessaria

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -7,11 +7,6 @@ import {
   mantineHtmlProps,
 } from "@mantine/core";
 
-export const metadata = {
-  title: "My Mantine app",
-  description: "I have followed setup instructions carefully",
-};
-
 export default function RootLayout({
   children,
 }: {
