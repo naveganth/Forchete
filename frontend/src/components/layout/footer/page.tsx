1daee402d040dc4df50f1b12a7f51f6fe90db747
Naveganth <lucas.navegant@gmail.com>
2025-06-23 17:25:49 -0300

    Corrigindo footer

--- frontend/src/components/layout/footer/page.tsx
+++ frontend/src/components/layout/footer/page.tsx
@@ -1,29 +1,47 @@
-'use client';
+"use client";
 
-import { IconBrandInstagram, IconBrandTwitter, IconBrandYoutube } from '@tabler/icons-react';
-import { ActionIcon, Container, Group, Text, useMantineColorScheme, useComputedColorScheme, Box, Anchor, Divider } from '@mantine/core';
-import { useEffect, useState } from 'react';
-import Link from 'next/link';
-import classes from './FooterLinks.module.css';
+import {
+  IconBrandInstagram,
+  IconBrandTwitter,
+  IconBrandYoutube,
+} from "@tabler/icons-react";
+import {
+  ActionIcon,
+  Container,
+  Group,
+  Text,
+  useMantineColorScheme,
+  useComputedColorScheme,
+  Box,
+  Anchor,
+  Divider,
+} from "@mantine/core";
+import { useEffect, useState } from "react";
+import Link from "next/link";
+import classes from "./FooterLinks.module.css";
 
 const navLinks = [
-  { label: 'Notícias', link: '/noticia' },
-  { label: 'Sobre', link: '/sobre' },
-  { label: 'Termos', link: '/termos' },
-  { label: 'Estatísticas', link: '/estatisticas' },
+  { label: "Notícias", link: "/noticia" },
+  { label: "Sobre", link: "/sobre" },
+  { label: "Termos", link: "/termos" },
+  { label: "Estatísticas", link: "/estatisticas" },
 ];
 
 export function FooterLinks() {
   const { setColorScheme } = useMantineColorScheme();
-  const computedColorScheme = useComputedColorScheme('dark', { getInitialValueInEffect: true });
+  const computedColorScheme = useComputedColorScheme("dark", {
+    getInitialValueInEffect: true,
+  });
   const [mounted, setMounted] = useState(false);
-  useEffect(() => { setMounted(true); }, []);
+  useEffect(() => {
+    setMounted(true);
+  }, []);
   const logoSrc = mounted
-    ? computedColorScheme === 'dark'
-      ? '/Dark.svg'
-      : '/Light.svg'
-    : '/Dark.svg';
-  const logoStyle = { opacity: mounted ? 1 : 0, transition: 'opacity 0.2s' };
+    ? computedColorScheme === "dark"
+      ? "/Dark.svg"
+      : "/Light.svg"
+    : "/Dark.svg";
+  const logoStyle = { opacity: mounted ? 1 : 0, transition: "opacity 0.2s" };
 
   const links = navLinks.map((link) => (
     <Anchor
@@ -40,28 +58,27 @@ export function FooterLinks() {
 
   return (
     <Box component="footer" className={classes.footer}>
-      <Container fluid className={classes.inner}>
-        <img src={logoSrc} alt="Logo" width={"auto"} height={40} style={logoStyle} />
-        
-        <Group gap="md" className={classes.links}>
+      <Container size={"xl"} className={classes.inner}>
+        <img
+          src={logoSrc}
+          alt="Logo"
+          width={"auto"}
+          height={40}
+          style={logoStyle}
+        />
+
+        <Group
+          gap="md"
+          justify="flex-end"
+          wrap="nowrap"
+          className={classes.links}
+        >
           {links}
         </Group>
-
-        <Group gap={0} justify="flex-end" wrap="nowrap">
-          <ActionIcon size="lg" color="gray" variant="subtle">
-            <IconBrandTwitter size={18} stroke={1.5} />
-          </ActionIcon>
-          <ActionIcon size="lg" color="gray" variant="subtle">
-            <IconBrandYoutube size={18} stroke={1.5} />
-          </ActionIcon>
-          <ActionIcon size="lg" color="gray" variant="subtle">
-            <IconBrandInstagram size={18} stroke={1.5} />
-          </ActionIcon>
-        </Group>
       </Container>
-      <Container>
-         <Divider my="md" />
-         <Text c="dimmed" size="sm" ta="center">
+      <Container size={"xl"}>
+        <Divider my="md" />
+        <Text c="dimmed" size="sm" ta="center">
           © 2024 Forchete. Todos os direitos reservados.
         </Text>
       </Container>
