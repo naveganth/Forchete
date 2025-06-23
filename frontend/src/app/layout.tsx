e4b98bcc6536de92cd1012fe82ad86e27ca4ed86
Naveganth <lucas.navegant@gmail.com>
2025-06-23 09:36:41 -0300

    Tema

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -17,7 +17,6 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&display=swap" rel="stylesheet" />
-        <link href="https://fonts.cdnfonts.com/css/friz-quadrata" rel="stylesheet"></link>
       </head>
       <body>
         <QueryProvider>
--- frontend/src/styles/theme.ts
+++ frontend/src/styles/theme.ts
@@ -1,26 +1,24 @@
 import { createTheme } from '@mantine/core';
 
 export const theme = createTheme({
-  fontFamily: 'Friz Quadrata, sans-serif',
+  fontFamily: ' ',
     headings: {
-        fontFamily: 'Inter, sans-serif',
-        fontWeight: '700',
+        fontFamily: '',
+        fontWeight: '',
     },
 
   colors: {
     brand: [
-      '#F0BBDD',
-      '#ED9BCF',
-      '#EC7CC3',
-      '#ED5DB8',
-      '#F13EAF',
-      '#F71FA7',
-      '#F610A0',
-      '#EA0BAA',
-      '#D5099B',
-      '#C3078F',
+      '#',
+      '#',
+      '#',
+      '#',
+      '#',
+      '#',
+      '#',
+      '#',
+      '#',
+      '#',
     ],
   },
-
-  primaryColor: 'brand',
 }); 
